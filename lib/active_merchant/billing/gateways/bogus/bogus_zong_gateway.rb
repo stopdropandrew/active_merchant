9d5d18057df642f6b68adaa57746b0dbe9c15a16
Ben Munat <bmunat@gmail.com>
2010-01-15 14:26:40 -0800

    add Zong bogus gateway

---
+++ lib/active_merchant/billing/gateways/bogus/bogus_zong_gateway.rb
@@ -0,0 +1,40 @@
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    class BogusZongGateway < Gateway
+      def initialize(options = {})
+        requires!(options, :customer_key)
+        @options = options
+        super
+      end 
+      
+      def lookup_price_points(options = {})
+        case options[:country_code]
+        when 'US'
+          results = [
+            {
+              :working_price => 5.75, 
+              :out_payment => 3.01, 
+              :item_ref  => 'item1', 
+              :zong_plus_only => false, 
+              :entrypoint_url => 'http://www.zong.com?purchaseKey=1234'
+            },
+            {
+              :working_price => 8.75, 
+              :out_payment => 5.01, 
+              :item_ref  => 'item2', 
+              :zong_plus_only => false, 
+              :entrypoint_url => 'http://www.zong.com?purchaseKey=8888'
+            }
+          ]
+          
+          Response.new(true, '', {:items => results }, :test => true )
+        when 'IQ'
+          Response.new(false, '', {:items => []}, :test => true )
+        else
+          Response.new(false, 'Invalid country', {}, :test => true )
+        end
+      end
+
+    end
+  end
+end
