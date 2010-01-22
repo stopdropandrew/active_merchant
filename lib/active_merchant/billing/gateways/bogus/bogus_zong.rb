cf1b7b72d4dddfee2da1cf6c0b8f6df71d9f3cf6
Ben Munat <bmunat@gmail.com>
2010-01-21 16:53:01 -0800

    made zong bogus gateway item_ref match real item_ref

--- lib/active_merchant/billing/gateways/bogus/bogus_zong.rb
+++ lib/active_merchant/billing/gateways/bogus/bogus_zong.rb
@@ -1,6 +1,8 @@
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     class BogusZongGateway < Gateway
+      self.supported_countries = ['US']
+
       def initialize(options = {})
         requires!(options, :customer_key)
         @options = options
@@ -14,14 +16,14 @@ module ActiveMerchant #:nodoc:
             {
               :working_price => 5.75, 
               :out_payment => 3.01, 
-              :item_ref  => 'item1', 
+              :item_ref  => '5.7500_USD', 
               :zong_plus_only => false, 
               :entrypoint_url => 'http://www.zong.com?purchaseKey=1234'
             },
             {
               :working_price => 8.75, 
               :out_payment => 5.01, 
-              :item_ref  => 'item2', 
+              :item_ref  => '8.7500_USD', 
               :zong_plus_only => false, 
               :entrypoint_url => 'http://www.zong.com?purchaseKey=8888'
             }
