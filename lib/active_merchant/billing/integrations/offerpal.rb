bee837d438be7a17e3cb280e960a739beaca297b
Ben Munat <bmunat@gmail.com>
2010-01-28 18:25:50 -0800

    added status_iframe_url to offerpal

--- lib/active_merchant/billing/integrations/offerpal.rb
+++ lib/active_merchant/billing/integrations/offerpal.rb
@@ -6,13 +6,19 @@ module ActiveMerchant #:nodoc:
       module Offerpal 
        
         IFRAME_URL = 'http://pub.myofferpal.com/<application_id>/showoffers.action?snuid=<user_id>'
-
+        STAUS_IFRAME_URL = 'http://pub.myofferpal.com/<application_id>/userstatus.action?snuid=<user_id>'
+        
         mattr_accessor :application_id
         mattr_accessor :secret_key
 
         def self.iframe_url(user_id)
           raise "application_id must be set to generate an iframe url" if application_id.nil?
           IFRAME_URL.sub('<application_id>', application_id).sub('<user_id>', user_id.to_s)
+        end
+        
+        def self.status_iframe_url(user_id)
+          raise "application_id must be set to generate an status_iframe url" if application_id.nil?
+          STAUS_IFRAME_URL.sub('<application_id>', application_id).sub('<user_id>', user_id.to_s)
         end
 
         def self.notification(post)
--- test/unit/integrations/offerpal_module_test.rb
+++ test/unit/integrations/offerpal_module_test.rb
@@ -16,4 +16,17 @@ class OfferpalModuleTest < Test::Unit::TestCase
   def test_iframe_url
     assert_equal "http://pub.myofferpal.com/153ddfb15ae1e37b7cf004b201c3e3fd/showoffers.action?snuid=1234", Offerpal.iframe_url(1234)
   end
+
+  def test_status_iframe_url_requires_application_id
+    old_app_id = Offerpal.application_id
+    Offerpal.application_id = nil
+    assert_raises RuntimeError do
+      Offerpal.status_iframe_url(1234)
+    end
+    Offerpal.application_id = old_app_id
+  end
+  
+  def test_status_iframe_url
+    assert_equal "http://pub.myofferpal.com/153ddfb15ae1e37b7cf004b201c3e3fd/userstatus.action?snuid=1234", Offerpal.status_iframe_url(1234)
+  end
 end 
