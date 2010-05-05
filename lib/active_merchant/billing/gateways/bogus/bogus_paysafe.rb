3a5645b25605175220456e6ab2587e2ca84f606d
stopdropandrew <stopdropandrew@gmail.com>
2010-05-05 09:17:14 -0700

    bogus gateway for easier testing

---
+++ lib/active_merchant/billing/gateways/bogus/bogus_paysafe.rb
@@ -0,0 +1,40 @@
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    class BogusPaysafeGateway < Gateway
+      GUID_CREATED = 'created-guid'
+      GUID_DISPOSED = 'disposed-guid'
+      GUID_EXPIRED = 'expired-guid'
+      
+      def initialize(options = {})
+        requires!(options, :merchant_id, :currency, :business_type)
+        @options = options
+        super
+      end 
+      
+      def authorize(options = {})
+        requires!(options, :transaction_id, :amount, :ok_url, :nok_url)
+        
+        Response.new(true, '', {}, :test => true )
+      end
+      
+      def check_transaction_status(options = {})
+        requires!(options, :transaction_id)
+        
+        case options[:transaction_id]
+        when GUID_CREATED
+          Response.new(true, '', { 'TransactionState' => PaysafeGateway::DISPOSITION_CREATED }, :test => true)
+        when GUID_DISPOSED
+          Response.new(true, '', { 'TransactionState' => PaysafeGateway::DISPOSITION_DISPOSED }, :test => true)
+        when GUID_EXPIRED
+          Response.new(true, '', { 'TransactionState' => PaysafeGateway::DISPOSITION_EXPIRED }, :test => true)
+        end
+      end
+      
+      def capture(options = {})
+        requires!(options, :transaction_id, :amount)
+
+        Response.new(true, '', {}, :test => true )
+      end
+    end
+  end
+end
